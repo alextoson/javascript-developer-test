4fce7337b15c50a8a64d582fe202349eac96d833
alextoson <alex@toson.com.au>
2021-11-02 19:58:42 +1100

    completed code challenge with all tests passing

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,25 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const results = [];
+
+  const getUrls = urls.map((url) => {
+    return httpGet(url);
+  });
+
+  await Promise.all(getUrls).then((responses) => {
+    responses.forEach((response) => {
+      const message = JSON.parse(response.body).message;
+
+      if (response.status === 200) {
+        results.push({ "Arnie Quote": message });
+      } else {
+        results.push({ FAILURE: message });
+      }
+    });
+  });
+
+  return results;
 };
 
 module.exports = {
